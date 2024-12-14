da3ce4a4a7bb4dee3c2d51295a28642077770dff
perwejalam03 <perwejalam03@gmail.com>
2024-12-14 12:51:57 +0530

    basic project setup

---
+++ vite-react/src/App.jsx
@@ -0,0 +1,12 @@
+import Demo from "./Demo";
+
+function App() {
+  return (
+    <>
+    <Demo/>
+    <h1>Hello</h1>
+    </>
+  )
+}
+
+export default App;
---
+++ vite-react/src/Demo.jsx
@@ -0,0 +1,4 @@
+function Demo(){
+    return <h1>Demo</h1>
+}
+export default Demo;
